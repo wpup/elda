a05eb8397a3c8a450837e321f3c6b163546a4e0e
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-08-18 00:19:40 +0200

    Add more tests

--- tests/EldaTest.php
+++ tests/EldaTest.php
@@ -29,6 +29,18 @@ class EldaTest extends \WP_UnitTestCase {
         $this->assertEquals( \Acme\Plugin_Loader::instance(), $acme );
     }
 
+    public function test_with_instance_exception() {
+        try {
+            $acme = Elda::boot( __DIR__ . '/fixtures/acme/acme-2-1.php', [
+                'instance' => 'Acme\\Plugin_Loader::instance2',
+                'src_dir'  => ''
+            ] );
+            $this->assertTrue( false );
+        } catch ( \Exception $e ) {
+            $this->assertEquals( '`Acme\\Plugin_Loader::instance2` is not callable', $e->getMessage() );
+        }
+    }
+
     public function test_load_files() {
         Elda::boot( __DIR__ . '/fixtures/acme/acme-3.php', [
             'files'    => [
@@ -56,6 +68,48 @@ class EldaTest extends \WP_UnitTestCase {
 
         $this->assertEquals( \Acme\Plugin_Loader::instance(), $acme );
         $this->assertEquals( \Acme\Plugin_Loader::instance(), Elda::make( __DIR__ . '/fixtures/acme/acme-5.php' ) );
+    }
+
+    public function test_make_exception() {
+        try {
+            $acme = Elda::make( __FILE__ );
+        } catch ( \Exception $e ) {
+            $this->assertEquals( sprintf( 'Identifier `%s` is not defined', plugin_basename( __FILE__ ) ), $e->getMessage() );
+        }
+    }
+
+    public function test_base_path_exception() {
+        try {
+            Elda::boot( false );
+        } catch ( \InvalidArgumentException $e ) {
+            $this->assertEquals( 'Invalid argument. `$base_path` must be string.', $e->getMessage() );
+        }
+    }
+
+    public function test_options_exceptions() {
+        try {
+            Elda::boot( __DIR__, [
+                'instance' => false
+            ] );
+        } catch ( \InvalidArgumentException $e ) {
+            $this->assertEquals( 'Invalid argument. `instance` must be string.', $e->getMessage() );
+        }
+
+        try {
+            Elda::boot( __DIR__, [
+                'namespace' => false
+            ] );
+        } catch ( \InvalidArgumentException $e ) {
+            $this->assertEquals( 'Invalid argument. `namespace` must be string.', $e->getMessage() );
+        }
+
+        try {
+            Elda::boot( __DIR__, [
+                'src_dir' => false
+            ] );
+        } catch ( \InvalidArgumentException $e ) {
+            $this->assertEquals( 'Invalid argument. `src_dir` must be string.', $e->getMessage() );
+        }
     }
 
 }
